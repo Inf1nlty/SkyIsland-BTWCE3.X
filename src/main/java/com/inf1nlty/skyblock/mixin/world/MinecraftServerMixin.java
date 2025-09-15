c7ad186a5e84bc5fd1b9b3e0e9f46ec827542a46
Inf1nity <1449896523@qq.com>
2025-09-15 21:36:29 +0800

    Add spawn chunk protect

--- src/main/java/com/inf1nlty/skyblock/mixin/world/MinecraftServerMixin.java
+++ src/main/java/com/inf1nlty/skyblock/mixin/world/MinecraftServerMixin.java
@@ -4,11 +4,14 @@ import com.inf1nlty.skyblock.command.SkyBlockCommand;
 import com.inf1nlty.skyblock.util.SkyBlockProtectionUtil;
 import com.inf1nlty.skyblock.util.VoidWorldMobCleaner;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.World;
 import net.minecraft.src.WorldServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
@@ -21,4 +24,20 @@ public abstract class MinecraftServerMixin {
 
         VoidWorldMobCleaner.onServerTick(world);
     }
+
+    @Inject(method = "isBlockProtected", at = @At("HEAD"), cancellable = true)
+    private static void injectBlockProtected(World world, int x, int y, int z, EntityPlayer player, CallbackInfoReturnable<Boolean> cir) {
+
+        if (player.capabilities.isCreativeMode) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        MinecraftServer server = ((WorldServer) world).getMinecraftServer();
+        int protection = server.getSpawnProtectionSize();
+        if (protection > 0 && Math.abs(x) <= protection && Math.abs(z) <= protection) {
+            cir.setReturnValue(true);
+        }
+    }
+
 }
